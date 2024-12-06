04aaeb9440412d6c87809060c0285ed98dbdec31
keronidas <djulian1992@gmail.com>
2024-12-06 03:49:25 +0100

    terminado

--- optimize-images.js
+++ optimize-images.js
@@ -8,7 +8,6 @@ import imageminPngquant from 'imagemin-pngquant';
     destination: 'src/assets/images',  // Sobrescribir en la misma carpeta
     plugins: [
       imageminJpegtran({ progressive: true, quality: [0.1, 0.3] }),
-
       imageminPngquant({ quality: [0.1, 0.3] })  
 
     ]
